902499dac6b14a63efda06dce76f3aff9c664cf1
Glenn Galicia <glenn.f.g.a@gmail.com>
2026-02-12 18:12:01 -0600

    technical: Includes packages to improve image quality - package.json use gulp-avif,gulp-webp and imagemin - gulpfile.js has a function to converse images to avif,webp and optimize. Watch if new images into folter src/img

--- 11-cafeteria/gulpfile.js
+++ 11-cafeteria/gulpfile.js
@@ -1,11 +1,16 @@
+// CSS & SASS
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const postcss = require('gulp-postcss')
 const autoprefixer = require('autoprefixer')
 
+// Images
+const imagemin = require('gulp-imagemin')
+const webp = require('gulp-webp')
+const avif = require('gulp-avif')
+
 function css(done) {
    // Compilar SaSS
-
    src('src/scss/app.scss')    // 1. Identificar archivo
       .pipe(sass({ outputStyle: 'expanded' })) // 2. Compilar
       .pipe(postcss([autoprefixer()])) // 3. Agregar prefijos
@@ -14,10 +19,42 @@ function css(done) {
    done()
 }
 
+function images(done) {
+   // Carga la carpeta imagenes
+   src('src/img/**/*')
+      .pipe(imagemin({ optimizationLevel: 3 }))
+      .pipe(dest('build/img'))
+
+   done()
+}
+
+function imgWebp(done) {
+   // Convierte imagenes a formato .webp
+   src('src/img/**/*.{jpg,png}')
+      .pipe(webp())
+      .pipe(dest('build/img'))
+   done()
+}
+
+function imgAvif(done) {
+   // Convierte imagenes a formato .avif
+   src(['src/img/**/*.{png,jpg}', '!src/img/**/*.svg'])
+      .pipe(avif({ quality: 50 }))
+      .pipe(dest('build/img'))
+   done()
+}
+
 function dev() {
    // Identifica cambios en el archivo y lo compila
    watch('src/scss/**/*.scss', css)
+   watch('src/img/**/*', images)
 }
+
 // Serie - inicia una tarea despues de otra
 // Parallel - inicia varias tareas al mismo tiempo
-exports.default = series(css, dev);
+exports.css = css;
+exports.dev = dev;
+exports.images = images;
+exports.imgWebp = imgWebp;
+exports.imgAvif = imgAvif;
+exports.default = series(images, imgWebp, css, dev);
